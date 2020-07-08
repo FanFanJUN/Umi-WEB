af1d070f382cf02395470447058c999ac2bde78b
sky1458@qq.com <sky1458@qq.com>
2020-07-08 09:35:52 +0800

    '没有token不赋值'

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,8 +4,11 @@ import { getAuthorization } from './user';
 
 const { request } = utils;
 request.defaults.baseURL = constants.BASE_URL;
-request.interceptors.request.use(function(config) {
-  config.headers['Authorization'] = getAuthorization();
+request.interceptors.request.use(function (config) {
+  const as = getAuthorization()
+  if (!!as) {
+    config.headers['Authorization'] = getAuthorization();
+  }
   return config;
 });
 
