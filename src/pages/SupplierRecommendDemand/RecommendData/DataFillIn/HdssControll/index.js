4e27f72c57c5a450613e7bf7fe3d0ca61d57e84d
Cai Li <1981824361@qq.com>
2020-09-08 17:47:49 +0800

    Tabs 页面 结构基本构建  Tabs分文件构造 基础结构

---
+++ src/pages/SupplierRecommendDemand/RecommendData/DataFillIn/HdssControll/index.js
@@ -0,0 +1,23 @@
+/*
+ * @Author: Li Cai
+ * @LastEditors: Li Cai
+ * @Date: 2020-09-08 16:58:22
+ * @LastEditTime: 2020-09-08 17:02:03
+ * @FilePath: /srm-sm-web/src/pages/SupplierRecommendDemand/RecommendData/DataFillIn/HdssControll/index.js
+ * @Description: 产品有害物质管控 tab
+ * @Connect: 1981824361@qq.com
+ */
+import React from 'react';
+import { Form } from 'antd';
+
+
+
+
+const HdssControll = (props) => {
+
+    return(
+        <h1>HdssControll 产品有害物质管控</h1>
+    )
+};
+
+export default Form.create()(HdssControll);
---
+++ src/pages/SupplierRecommendDemand/RecommendData/DataFillIn/Other/index.js
@@ -0,0 +1,23 @@
+/*
+ * @Author: Li Cai
+ * @LastEditors: Li Cai
+ * @Date: 2020-09-08 16:58:13
+ * @LastEditTime: 2020-09-08 17:03:25
+ * @FilePath: /srm-sm-web/src/pages/SupplierRecommendDemand/RecommendData/DataFillIn/Other/index.js
+ * @Description: 其他附加资料
+ * @Connect: 1981824361@qq.com
+ */
+import React from 'react';
+import { Form } from 'antd';
+
+
+
+
+const Other = (props) => {
+
+    return(
+        <h1>Other 其他附加资料</h1>
+    )
+};
+
+export default Form.create()(Other);
