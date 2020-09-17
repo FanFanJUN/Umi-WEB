e3f8cbc3cf1bbd6fbed47fce217e1ecc1c9c9894
cjy <576992125@qq.com>
2020-09-17 16:04:45 +0800

    技术资料分享界面上传附件修改

--- src/pages/QualitySynergy/compoent/Upload/index.js
+++ src/pages/QualitySynergy/compoent/Upload/index.js
@@ -321,7 +321,7 @@ class UploadFile extends React.Component {
             beforeUpload={this.beforeUpload}
             showUploadList={false}
             fileList={this.state.fileList}
-            action={BASE_URL + baseUrl + "/supplierRegister/uploadNoAuth"}
+            action={'/service.api' + baseUrl + "/supplierRegister/uploadNoAuth"}
             headers={this.getHeaders()}
             onChange={this.handleChange}
             style={{width: '100%'}}
