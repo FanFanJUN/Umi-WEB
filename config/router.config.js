13596a4444937bbcbdc68ff98be019952d2f3d69
SmallCuiCui <huangyc@goktech.cn>
2020-09-21 14:47:17 +0800

    注释新加页面

--- config/router.config.js
+++ config/router.config.js
@@ -31,7 +31,7 @@ export default [
       { path: '/qualitySynergy/LimitMaterial', component: './QualitySynergy/mainData/LimitMaterial', title: '环保标准限用物质对应关系-主数据' },
       { path: '/qualitySynergy/BusinessUnitToBU', component: './QualitySynergy/mainData/BusinessUnitToBU', title: '业务模块对业务单元主数据-主数据' },
       { path: '/qualitySynergy/BU', component: './QualitySynergy/mainData/BU', title: '业务单元主数据-主数据' },
-      { path: '/qualitySynergy/EPStatement', component: './QualitySynergy/mainData/EPStatement', title: '环保数据字典表' },
+      // { path: '/qualitySynergy/EPStatement', component: './QualitySynergy/mainData/EPStatement', title: '环保数据字典表' },
 
     ]
   },
